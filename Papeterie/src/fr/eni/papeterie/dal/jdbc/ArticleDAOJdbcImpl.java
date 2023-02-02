b2fc6dd9e0f485d6a97bcb878bcd99338a13a087
Thomas LHERAULT <tlherault2022@campus-eni.fr>
2023-02-02 18:20:51 +0100

    update

--- Papeterie/src/fr/eni/papeterie/dal/jdbc/ArticleDAOJdbcImpl.java
+++ Papeterie/src/fr/eni/papeterie/dal/jdbc/ArticleDAOJdbcImpl.java
@@ -27,8 +27,10 @@ public class ArticleDAOJdbcImpl {
 	private static final String TYPE_STYLO = "STYLO";
 	private static final String TYPE_RAMETTE = "RAMETTE";
 	
-	//private static final String sqlSelectAll = "select idArticle, reference, marque, designation, prixUnitaire, qteStock, grammage, couleur, type from articles";
+	private static final String sqlSelectAll = "select * from articles";
+	private static final String sqlUpdate = "update articles set reference=?,marque=?,designation=?,prixUnitaire=?,qteStock=?,grammage=?,couleur=? where idArticle=?";
 	private static final String sqlInsert = "insert into articles(reference,marque,designation,prixUnitaire,qteStock,type,grammage,couleur) values(?,?,?,?,?,?,?,?)";
+	private static final String sqlDelete = "delete from articles where reference=?";
 	
 	private Connection connection;
 	
@@ -61,40 +63,119 @@ public class ArticleDAOJdbcImpl {
 	}
 	
 	public List<Article> selectAll() {
-		return null;
+		List<Article> articles = new ArrayList<Article>();
+		Connection connection = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			connection = getConnection();
+			stmt = connection.createStatement();
+			rs = stmt.executeQuery(sqlSelectAll);
+			Article article = null;
+			while (rs.next()) {
+				if (TYPE_STYLO.equals(rs.getString("type"))) {
+					article = new Stylo(rs.getInt("idArticle"), rs.getString("marque"), rs.getString("reference").trim(), rs.getString("designation"),
+							rs.getFloat("prixUnitaire"), rs.getInt("qteStock"), rs.getString("couleur"));
+				}
+				if (TYPE_RAMETTE.equalsIgnoreCase(rs.getString("type").trim())) {
+					article = new Ramette(rs.getInt("idArticle"), rs.getString("marque"), rs.getString("reference").trim(), rs.getString("designation"), 
+							rs.getFloat("prixUnitaire"), rs.getInt("qteStock"), rs.getInt("grammage"));
+				}
+				articles.add(article);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			closeConnection();
+		}
+		return articles;
 	}
 	
 	public void update(Article article) {
-		
+		Connection connection = null;
+		PreparedStatement stmt = null;
+		try {
+			connection = getConnection();
+			stmt = connection.prepareStatement(sqlUpdate);
+			
+			stmt.setString(1, article.getReference());
+			stmt.setString(2, article.getMarque());
+			stmt.setString(3, article.getDesignation());
+			stmt.setFloat(4, article.getPrixUnitaire());
+			stmt.setInt(5, article.getQteStock());
+			//Ajoute les variables propres aux ramettes
+			if(article instanceof Ramette) {
+				Ramette r = (Ramette) article;
+				stmt.setString(6, TYPE_RAMETTE);
+				stmt.setInt(7, r.getGrammage());
+				stmt.setNull(8, Types.VARCHAR);
+			}
+			//Ajoute les variables propres aux stylos
+			else if(article instanceof Stylo) {
+				Stylo s = (Stylo) article;
+				stmt.setString(6, TYPE_STYLO);
+				stmt.setNull(7, Types.INTEGER);
+				stmt.setString(8, s.getCouleur());
+			}
+			
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			closeConnection();
+		}
 	}
 	
 	public void insert(Article article) throws DALException {
-		Connection cnx = null;
-		PreparedStatement rqt = null;
+		Connection connection = null;
+		PreparedStatement stmt = null;
 		try {
-			cnx = getConnection();
-			rqt = cnx.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
-			rqt.setString(1, article.getReference());
-			rqt.setString(2, article.getMarque());
-			rqt.setString(3, article.getDesignation());
-			rqt.setFloat(4, article.getPrixUnitaire());
-			rqt.setInt(5, article.getQteStock());
-			if (article instanceof Ramette) {
+			//Récupération de la connection
+			connection = getConnection();
+			//Préparation de la requête d'insertion
+			stmt = connection.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
+			//Met des valeurs aux variables
+			stmt.setString(1, article.getReference());
+			stmt.setString(2, article.getMarque());
+			stmt.setString(3, article.getDesignation());
+			stmt.setFloat(4, article.getPrixUnitaire());
+			stmt.setInt(5, article.getQteStock());
+			//Ajoute les variables propres aux ramettes
+			if(article instanceof Ramette) {
 				Ramette r = (Ramette) article;
-				rqt.setString(6, TYPE_RAMETTE);
-				rqt.setInt(7, r.getGrammage());
-				rqt.setNull(8, Types.VARCHAR);
+				stmt.setString(6, TYPE_RAMETTE);
+				stmt.setInt(7, r.getGrammage());
+				stmt.setNull(8, Types.VARCHAR);
 			}
-			if (article instanceof Stylo) {
+			//Ajoute les variables propres aux stylos
+			else if(article instanceof Stylo) {
 				Stylo s = (Stylo) article;
-				rqt.setString(6, TYPE_STYLO);
-				rqt.setNull(7, Types.INTEGER);
-				rqt.setString(8, s.getCouleur());
+				stmt.setString(6, TYPE_STYLO);
+				stmt.setNull(7, Types.INTEGER);
+				stmt.setString(8, s.getCouleur());
 			}
 
-			int nbRows = rqt.executeUpdate();
-			if (nbRows == 1) {
-				ResultSet rs = rqt.getGeneratedKeys();
+			int nbRows = stmt.executeUpdate();
+			if(nbRows == 1) {
+				ResultSet rs = stmt.getGeneratedKeys();
 				if (rs.next()) {
 					article.setIdArticle(rs.getInt(1));
 				}
@@ -105,8 +186,8 @@ public class ArticleDAOJdbcImpl {
 			throw new DALException("Insert article failed - " + article, e);
 		} finally {
 			try {
-				if (rqt != null) {
-					rqt.close();
+				if (stmt != null) {
+					stmt.close();
 				}
 				
 			} catch (SQLException e) {
@@ -117,8 +198,28 @@ public class ArticleDAOJdbcImpl {
 		}
 	}
 	
-	public void delete(Integer id) {
-		
+	public void delete(int id) {
+		Connection connection = null;
+		PreparedStatement stmt = null;
+		try {
+			connection = getConnection();
+			stmt = connection.prepareStatement(sqlDelete);
+			stmt.setInt(1, id);
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			closeConnection();
+		}
 	}
 
 }
